dc511c7ce3070e4b106b8d22c511c30eddf3143a
matthewdizon <dominic.dizon@obf.ateneo.edu>
2023-01-08 02:54:16 +0800

    added temporary slug components

---
+++ pages/users/[slug].js
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Layout from "../../components/layout";
+
+function PartSlug() {
+  const [data, setData] = useState(null);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  useEffect(() => {
+    async function fetchData() {
+      // Make sure slug is defined before using it
+      if (slug) {
+        // Retrieve the JWT from local storage
+        const jwt = localStorage.getItem("accessToken");
+
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/parts/` + slug,
+          {
+            headers: {
+              Authorization: `Bearer ${jwt}`,
+            },
+          }
+        );
+        try {
+          const data = await res.json();
+          setData(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    fetchData();
+  }, [slug]);
+
+  console.log(router.query);
+  console.log(data);
+
+  return (
+    <Layout>
+      <p>{slug}</p>
+    </Layout>
+  );
+}
+
+export default PartSlug;
