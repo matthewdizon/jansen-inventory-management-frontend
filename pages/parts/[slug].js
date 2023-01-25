f237db9b548a126a6218ed1357b2ba0cd0d6658e
matthewdizon <dominic.dizon@obf.ateneo.edu>
2023-01-26 02:53:02 +0800

    allowed updating parts

--- pages/parts/[slug].js
+++ pages/parts/[slug].js
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Layout from "../../components/layout";
 import Link from "next/link";
 
 function PartSlug() {
   const [data, setData] = useState(null);
   const [showDeleteModal, setShowDeleteModal] = useState(null);
+  const [showUpdateModal, setShowUpdateModal] = useState(null);
 
   const router = useRouter();
   const { slug } = router.query;
@@ -142,6 +143,132 @@ function PartSlug() {
     );
   };
 
+  const UpdateModal = ({
+    setShowUpdateModal,
+    currName,
+    currSupplier,
+    currQuantity,
+    currThreshold,
+  }) => {
+    const [name, setName] = useState(currName);
+    const [supplier, setSupplier] = useState(currSupplier);
+    const [quantity, setQuantity] = useState(currQuantity);
+    const [quantityThreshold, setQuantityThreshold] = useState(currThreshold);
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const jwt = localStorage.getItem("accessToken");
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/parts/` + slug,
+        {
+          method: "PATCH",
+          body: JSON.stringify({
+            slug,
+            name,
+            supplier,
+            quantity,
+            quantityThreshold,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${jwt}`,
+          },
+        }
+      );
+
+      if (res.ok) {
+        console.log(res);
+        setShowUpdateModal(false);
+      }
+    };
+
+    return (
+      <div
+        className={`fixed inset-0 flex items-center justify-center`}
+        style={{ background: "rgba(50, 50, 50, 0.8)" }}
+      >
+        <form
+          className="bg-white p-16 rounded-2xl relative"
+          onSubmit={handleSubmit}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => setShowUpdateModal(false)}
+            className="absolute top-6 right-6 hover:cursor-pointer"
+          >
+            <path
+              d="M16.3394 9.32245C16.7434 8.94589 16.7657 8.31312 16.3891 7.90911C16.0126 7.50509 15.3798 7.48283 14.9758 7.85938L12.0497 10.5866L9.32245 7.66048C8.94589 7.25647 8.31312 7.23421 7.90911 7.61076C7.50509 7.98731 7.48283 8.62008 7.85938 9.0241L10.5866 11.9502L7.66048 14.6775C7.25647 15.054 7.23421 15.6868 7.61076 16.0908C7.98731 16.4948 8.62008 16.5171 9.0241 16.1405L11.9502 13.4133L14.6775 16.3394C15.054 16.7434 15.6868 16.7657 16.0908 16.3891C16.4948 16.0126 16.5171 15.3798 16.1405 14.9758L13.4133 12.0497L16.3394 9.32245Z"
+              fill="currentColor"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21Z"
+              fill="currentColor"
+            />
+          </svg>
+          <p className="font-bold text-xl">Add New Payment</p>
+          <div className="grid gap-8 py-8">
+            <div className="grid grid-cols-2">
+              <p>Name</p>
+              <input
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                placeholder="Name"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Supplier</p>
+              <input
+                type="text"
+                onChange={(e) => setSupplier(e.target.value)}
+                value={supplier}
+                placeholder="supplier"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Quantity</p>
+              <input
+                type="number"
+                onChange={(e) => setQuantity(e.target.value)}
+                value={quantity}
+                placeholder="quantity"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+            <div className="grid grid-cols-2">
+              <p>Quantity Threshold</p>
+              <input
+                type="number"
+                onChange={(e) => setQuantityThreshold(e.target.value)}
+                value={quantityThreshold}
+                placeholder="quantity threshold"
+                className="px-4 py-2 rounded-xl border-2 border-gray-200"
+              />
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-[#3da9fc] text-[#fffffe] px-4 py-2 rounded-lg hover:shadow-lg uppercase"
+            >
+              Update Part
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
   const {
     quantity,
     quantityThreshold,
@@ -159,28 +286,63 @@ function PartSlug() {
         {showDeleteModal && (
           <DeleteModal setShowDeleteModal={setShowDeleteModal} />
         )}
+        {showUpdateModal && (
+          <UpdateModal
+            setShowUpdateModal={setShowUpdateModal}
+            currName={name}
+            currSupplier={supplier}
+            currQuantity={quantity}
+            currThreshold={quantityThreshold}
+          />
+        )}
         <p>Part ID: {slug}</p>
-        <div
-          className="flex items-center bg-red-400 text-[#fffffe] rounded-lg p-2 px-4 gap-2 hover:opacity-50 duration-500 transition hover:cursor-pointer"
-          onClick={() => setShowDeleteModal(true)}
-        >
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <div className="flex gap-4">
+          <div
+            className="flex items-center bg-blue-400 text-[#fffffe] rounded-lg p-2 px-4 gap-2 hover:opacity-50 duration-500 transition hover:cursor-pointer"
+            onClick={() => setShowUpdateModal(true)}
           >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
-              fill="currentColor"
-            />
-            <path d="M9 9H11V17H9V9Z" fill="currentColor" />
-            <path d="M13 9H15V17H13V9Z" fill="currentColor" />
-          </svg>
-          <p>Delete</p>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M21.2635 2.29289C20.873 1.90237 20.2398 1.90237 19.8493 2.29289L18.9769 3.16525C17.8618 2.63254 16.4857 2.82801 15.5621 3.75165L4.95549 14.3582L10.6123 20.0151L21.2189 9.4085C22.1426 8.48486 22.338 7.1088 21.8053 5.99367L22.6777 5.12132C23.0682 4.7308 23.0682 4.09763 22.6777 3.70711L21.2635 2.29289ZM16.9955 10.8035L10.6123 17.1867L7.78392 14.3582L14.1671 7.9751L16.9955 10.8035ZM18.8138 8.98525L19.8047 7.99429C20.1953 7.60376 20.1953 6.9706 19.8047 6.58007L18.3905 5.16586C18 4.77534 17.3668 4.77534 16.9763 5.16586L15.9853 6.15683L18.8138 8.98525Z"
+                fill="currentColor"
+              />
+              <path
+                d="M2 22.9502L4.12171 15.1717L9.77817 20.8289L2 22.9502Z"
+                fill="currentColor"
+              />
+            </svg>
+            <p>Update</p>
+          </div>
+          <div
+            className="flex items-center bg-red-400 text-[#fffffe] rounded-lg p-2 px-4 gap-2 hover:opacity-50 duration-500 transition hover:cursor-pointer"
+            onClick={() => setShowDeleteModal(true)}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+                fill="currentColor"
+              />
+              <path d="M9 9H11V17H9V9Z" fill="currentColor" />
+              <path d="M13 9H15V17H13V9Z" fill="currentColor" />
+            </svg>
+            <p>Delete</p>
+          </div>
         </div>
       </div>
       <div className="grid grid-cols-2 gap-8">
