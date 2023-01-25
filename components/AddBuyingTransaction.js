e35d9fd91006df185b660a38d8b403e91c1996cc
matthewdizon <dominic.dizon@obf.ateneo.edu>
2023-01-25 16:24:11 +0800

    refactored buying tx form and allowed deleting slug

--- components/AddBuyingTransaction.js
+++ components/AddBuyingTransaction.js
@@ -6,7 +6,7 @@ export default function AddBuyingTransactionForm() {
     new Date().toISOString().substring(0, 10)
   );
   const [items, setItems] = useState([
-    { part: "", quantity: "", price: "", supplier: "" },
+    { isNew: false, part: "", quantity: "", price: "", supplier: "" },
   ]);
   const [allItems, setAllItems] = useState([]);
   const [itemOptions, setItemOptions] = useState([]);
@@ -17,13 +17,24 @@ export default function AddBuyingTransactionForm() {
 
   const handleChange = (e, index) => {
     const { name, value } = e.target;
-    const newItems = [...items];
-    newItems[index][name] = value;
-    setItems(newItems);
+    console.log(index, name, value);
+    if (name === "isNew") {
+      var isTrue = value === "false";
+      const newItems = [...items];
+      newItems[index][name] = isTrue;
+      setItems(newItems);
+    } else {
+      const newItems = [...items];
+      newItems[index][name] = value;
+      setItems(newItems);
+    }
   };
 
   const handleAddItem = () => {
-    setItems([...items, { part: "", quantity: "", price: "", supplier: "" }]);
+    setItems([
+      ...items,
+      { isNew: false, part: "", quantity: "", price: "", supplier: "" },
+    ]);
   };
 
   const handleRemoveItem = (index) => {
@@ -67,8 +78,6 @@ export default function AddBuyingTransactionForm() {
     fetchData();
   }, []);
 
-  console.log("HERE YOU GO", allItems);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -95,11 +104,13 @@ export default function AddBuyingTransactionForm() {
 
     if (res.ok) {
       // Set success message
-      setMessage("Successfully added part");
+      setMessage("Successfully added Buying Transaction");
       setShowMessage(true);
 
       setTransactionDate(new Date().toISOString().substring(0, 10));
-      setItems([{ part: "", quantity: "", price: "", supplier: "" }]);
+      setItems([
+        { isNew: false, part: "", quantity: "", price: "", supplier: "" },
+      ]);
       setDeliveryFee(0);
     } else {
       // Set error message
@@ -109,86 +120,135 @@ export default function AddBuyingTransactionForm() {
   };
 
   return (
-    <>
+    <div className="bg-white rounded-xl p-8 px-12">
+      <p className="font-bold text-lg">Add New Buying Transaction</p>
       <form className="grid gap-2" onSubmit={handleSubmit}>
-        <label>date of transaction</label>
-        <input
-          type="date"
-          onChange={(e) => setTransactionDate(e.target.value)}
-          value={transactionDate}
-          className="px-4 py-2 rounded-xl"
-        />
-        <input
-          type="number"
-          name="delivery fee"
-          className="px-4 py-2 rounded-xl"
-          placeholder="delivery fee"
-          // value={item.quantity}
-          onChange={(e) => setDeliveryFee(e.target.value)}
-        />
-        <label>items</label>
-        {items.map((item, index) => (
-          <div key={index} className="flex gap-2">
-            <input
-              list="item-options"
-              name="part"
-              className="px-4 py-2 rounded-xl"
-              placeholder="item name"
-              value={item.part}
-              onChange={(e) => handleChange(e, index)}
-              onInput={handleSearch}
-            />
-            <datalist id="item-options">
-              {itemOptions.map((item) => (
-                <option key={item.name} value={item.name} label={item.name} />
-              ))}
-            </datalist>
-            <input
-              type="number"
-              name="quantity"
-              className="px-4 py-2 rounded-xl"
-              placeholder="quantity"
-              value={item.quantity}
-              onChange={(e) => handleChange(e, index)}
-            />
-            <input
-              type="number"
-              name="price"
-              className="px-4 py-2 rounded-xl"
-              placeholder="price"
-              value={item.price}
-              onChange={(e) => handleChange(e, index)}
-            />
-            <input
-              type="text"
-              name="supplier"
-              className="px-4 py-2 rounded-xl"
-              placeholder="supplier"
-              value={item.supplier}
-              onChange={(e) => handleChange(e, index)}
-            />
-            {items.length > 1 && (
-              <button type="button" onClick={() => handleRemoveItem(index)}>
-                -
-              </button>
-            )}
-            {index === items.length - 1 && (
-              <button type="button" onClick={handleAddItem}>
-                +
-              </button>
-            )}
+        <div className="grid grid-cols-2 items-center">
+          <label>date of transaction</label>
+          <input
+            type="date"
+            onChange={(e) => setTransactionDate(e.target.value)}
+            value={transactionDate}
+            className="px-4 py-2 rounded-xl border-2 border-gray-200"
+          />
+        </div>
+        <div className="grid grid-cols-2 items-center">
+          <p>Delivery Fee</p>
+          <input
+            type="number"
+            name="delivery fee"
+            className="px-4 py-2 rounded-xl border-2 border-gray-200"
+            placeholder="delivery fee"
+            onChange={(e) => setDeliveryFee(e.target.value)}
+          />
+        </div>
+        <div className="grid gap-4">
+          <div className="flex gap-4 items-center">
+            <label>Items</label>
+            <button
+              type="button"
+              onClick={handleAddItem}
+              className="bg-gray-200 rounded-xl p-2 px-6"
+            >
+              Add Another Item
+            </button>
           </div>
-        ))}
-        <button
-          type="submit"
-          className="bg-[#3da9fc] text-[#fffffe] px-4 py-2 rounded-lg hover:shadow-lg uppercase"
-        >
-          Submit
-        </button>
+          <div className="grid gap-2">
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className="grid grid-cols-5 gap-2 relative items-center"
+              >
+                <div className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    name="isNew"
+                    onChange={(e) => handleChange(e, index)}
+                    value={item.isNew}
+                    checked={item.isNew}
+                  />
+                  <p>New Item</p>
+                </div>
+                {item.isNew ? (
+                  <input
+                    name="part"
+                    className="px-4 py-2 rounded-xl border-2 border-gray-200"
+                    placeholder="item name"
+                    value={item.part}
+                    onChange={(e) => handleChange(e, index)}
+                  />
+                ) : (
+                  <select
+                    className="h-full rounded-lg p-4"
+                    value={item.part}
+                    name="part"
+                    onChange={(e) => handleChange(e, index)}
+                    required
+                  >
+                    <option value="">Select Part</option>
+                    {allItems.map((item, index) => {
+                      return (
+                        <option value={item._id} key={index}>
+                          {item.name} ({item.supplier})
+                        </option>
+                      );
+                    })}
+                  </select>
+                )}
+                <input
+                  type="number"
+                  name="quantity"
+                  className="px-4 py-2 rounded-xl border-2 border-gray-200"
+                  placeholder="quantity"
+                  value={item.quantity}
+                  onChange={(e) => handleChange(e, index)}
+                  required
+                />
+                <input
+                  type="number"
+                  name="price"
+                  className="px-4 py-2 rounded-xl border-2 border-gray-200"
+                  placeholder="price"
+                  value={item.price}
+                  onChange={(e) => handleChange(e, index)}
+                  required
+                />
+                {item.isNew && (
+                  <input
+                    name="supplier"
+                    className="px-4 py-2 rounded-xl border-2 border-gray-200"
+                    placeholder="supplier"
+                    value={item.supplier}
+                    onChange={(e) => handleChange(e, index)}
+                  />
+                )}
+                {items.length > 1 && (
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveItem(index)}
+                    className="absolute -right-4"
+                  >
+                    -
+                  </button>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-[#3da9fc] text-[#fffffe] px-8 py-2 rounded-lg hover:shadow-lg uppercase max-w-max"
+          >
+            Submit
+          </button>
+        </div>
       </form>
       {showMessage && (
-        <div className="bg-blue-500 text-white p-4 rounded-xl">{message}</div>
+        <div className="bg-blue-500 text-white p-4 rounded-xl my-4">
+          {message}
+        </div>
       )}
-    </>
+    </div>
   );
 }
--- pages/transactions/[slug].js
+++ pages/transactions/[slug].js
@@ -9,6 +9,7 @@ import useSWR from "swr";
 function PartSlug() {
   const [data, setData] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(null);
 
   const router = useRouter();
   const { slug } = router.query;
@@ -33,7 +34,23 @@ function PartSlug() {
     }
   );
 
-  console.log("swrswrswr", swrData);
+  const deleteTransaction = async (id) => {
+    const jwt = localStorage.getItem("accessToken");
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_SERVER}/api/transactions/${type}/` +
+        id,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwt}`,
+        },
+      }
+    );
+
+    if (res.ok) router.push("/transactions");
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -65,6 +82,67 @@ function PartSlug() {
 
   console.log(router.query);
   console.log(data);
+
+  const DeleteModal = ({ setShowDeleteModal }) => {
+    return (
+      <div className="fixed bottom-0 inset-x-0 px-4 pb-6 z-20">
+        <div className="fixed inset-0 transition-opacity">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <svg
+                  className="h-6 w-6 text-red-600"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                  />
+                </svg>
+              </div>
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  Confirm Delete
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm leading-5 text-gray-500">
+                    Are you sure you want to delete this item?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+              <button
+                type="button"
+                className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-800"
+                onClick={() => deleteTransaction(slug)}
+              >
+                Confirm
+              </button>
+            </span>
+            <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+              <button
+                type="button"
+                className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                onClick={() => setShowDeleteModal(false)}
+              >
+                Cancel
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   const SellingData = () => {
     const { collectionDate, customer, date, items, payments, total, user } =
@@ -273,7 +351,34 @@ function PartSlug() {
 
   return (
     <Layout>
-      <p className="mb-8">Invoice: {slug}</p>
+      <div className="flex items-center mb-8 justify-between">
+        {showDeleteModal && (
+          <DeleteModal setShowDeleteModal={setShowDeleteModal} />
+        )}
+        <p>Invoice: {slug}</p>
+        <div
+          className="flex items-center bg-red-400 text-[#fffffe] rounded-lg p-2 px-4 gap-2 hover:opacity-50 duration-500 transition hover:cursor-pointer"
+          onClick={() => setShowDeleteModal(true)}
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M17 5V4C17 2.89543 16.1046 2 15 2H9C7.89543 2 7 2.89543 7 4V5H4C3.44772 5 3 5.44772 3 6C3 6.55228 3.44772 7 4 7H5V18C5 19.6569 6.34315 21 8 21H16C17.6569 21 19 19.6569 19 18V7H20C20.5523 7 21 6.55228 21 6C21 5.44772 20.5523 5 20 5H17ZM15 4H9V5H15V4ZM17 7H7V18C7 18.5523 7.44772 19 8 19H16C16.5523 19 17 18.5523 17 18V7Z"
+              fill="currentColor"
+            />
+            <path d="M9 9H11V17H9V9Z" fill="currentColor" />
+            <path d="M13 9H15V17H13V9Z" fill="currentColor" />
+          </svg>
+          <p>Delete</p>
+        </div>
+      </div>
       {type === "selling" ? <SellingData /> : <BuyingData />}
     </Layout>
   );
